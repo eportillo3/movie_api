2425d1e6631b00f07d0a1c6e5c9f075cfdd560fe
Erik Portillo <erikportillo1@gmail.com>
2021-02-21 13:07:31 -0500

    updated cors policy

--- index.js
+++ index.js
@@ -40,16 +40,18 @@ const {
   validationResult
 } = require('express-validator');
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = 'The CORs policy for this application doesn\'t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       let message = 'The CORs policy for this application doesn\'t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+
+app.use(cors());
 
 
 // GET requests
