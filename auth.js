f3c148ebb756f7e7982ab31a611f287446e3f65f
Erik Portillo <erikportillo1@gmail.com>
2021-05-06 17:22:46 -0400

    added documentation with jsdoc

--- auth.js
+++ auth.js
@@ -1,26 +1,33 @@
-const jwtSecret = 'your_jwt_secret';
+const jwtSecret = "your_jwt_secret";
 
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
+const jwt = require("jsonwebtoken"),
+  passport = require("passport");
 
-require('./passport');
+require("./passport");
 
+/**
+ * Function to generate authentication token with expiration and algorithm settings
+ * @param {string} user
+ * @returns JWTToken
+ */
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
-    subject: user.Username,
-    expiresIn: '7d',
-    algorithm: 'HS256'
+    subject: user.Username, // username being encoded in jwt
+    expiresIn: "7d", // specifies token expiration in days
+    algorithm: "HS256", // algorithm used to sign or encode the values of the jwt
   });
-}
+};
 
-// POST login
+/**
+ * API endpoint which authenticates a user after entering login credentials
+ */
 module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
+  router.post("/login", (req, res) => {
+    passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
-          message: 'Something is not right',
-          user: user
+          message: "Something is not right",
+          user: user,
         });
       }
       req.login(user, { session: false }, (error) => {
@@ -28,8 +35,8 @@ module.exports = (router) => {
           res.send(error);
         }
         let token = generateJWTToken(user.toJSON());
-        return res.json({ user, token});
+        return res.json({ user, token });
       });
     })(req, res);
   });
-}
+};
